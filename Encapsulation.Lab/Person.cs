21da9204a13b09212768a0cf158836d9be18b77a
Vinh Hoang <nhuvinh.hoang@gmail.com>
2020-03-30 15:40:48 +0700

    Add Encapsulation.Lab Problem 1

---
+++ Encapsulation.Lab/Person.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation.Lab
+{
+	class Person
+	{
+		private string FirstName;
+		private string LastName;
+		private int Age;
+
+		public Person(string firstName, string lastName, int age)
+		{
+			FirstName = firstName;
+			LastName = lastName;
+			Age = age;
+		}
+
+		public override string ToString()
+		{
+			return FirstName + " " + LastName + " is " + Age + " years old.";
+		}
+
+		public string GetFirstName()
+		{
+			return FirstName;
+		}
+
+		public int GetAge()
+		{
+			return Age;
+		}
+	}
+}
